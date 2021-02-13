87006ac03084df191cfeef1538f96be80c234ac4
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-02-12 20:31:16 -0500

    Fixed bug

--- get_git_report_and_error_if_uncommited_changes.m
+++ get_git_report_and_error_if_uncommited_changes.m
@@ -23,17 +23,7 @@ function breadcrumb_string = get_git_report_and_error_if_uncommited_changes(repo
     
     % Get the recent git log
     git_log = system_with_error_handling('git log --graph --oneline --max-count 10 | cat') ;
-    
-    % If the destination folder exists, error out
-    install_folder_path = ...
-      sprintf('/groups/mousebrainmicro/mousebrainmicro/pipeline-systems/pipeline-%s/apps/pipeline-featmatch-%s', a_or_b, short_commit_hash) ;
-    if exist(install_folder_path, 'file') ,
-        error('Install folder %s already exists, please move or delete before installing new one') ;
-    end
-    
-    % Create the install folder, and any needed parent folders
-    ensure_folder_exists(install_folder_path) ;
-    
+        
     % Write a file with the commit hash into the folder, for good measure
     breadcrumb_string = sprintf('Source repo:\n%s\n\nCommit hash:\n%s\n\nRemote info:\n%s\n\nGit status:\n%s\n\nGit log:\n%s\n\n', ...
                                 source_repo_folder_path, ...
