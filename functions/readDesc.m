6418b3c111ea657faba0a33e6e88c0a504e6d140
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-03-12 22:12:06 -0500

    Moved functional part of pointmatch.m into pointmatch_core.m

--- functions/readDesc.m
+++ functions/readDesc.m
@@ -1,74 +1,14 @@
-function des = readDesc(inputfolder,desc_ch,ext_desc)
-% %%
-% if nargin<2
-%     desc_ch = {'1'};
-%     ext_desc = 'txt';
-% end
-% if nargin<3
-%     ext_desc = 'txt';
-% end
-% myfiles = dir(fullfile(inputfolder,['*.',ext_desc]));
-% fid = fopen(fullfile(myfiles(1).folder,myfiles(1).name));
-% tLines = fgets(fid);
-% numch = length(desc_ch);
-% delimiter = ' '; %or whatever
-% if length(tLines)>1
-%     numCols = numel(strfind(tLines,delimiter)) + 1;
-%     fclose(fid);
-% else
-%     des = [];
-%     return
-%     % error('EMPTY DESCRIPTOR FILE !!')
-% end
-% format = repmat('%f ',1,numCols);format(end)=[];
-% %% if there are multi-channel descriptor, decide what to do
-% %% TODO
-% clear des
-% thesechannels = desc_ch;
-% 
-% for idx_=1:length(thesechannels)
-%     idx = thesechannels{idx_};
-%     ii = str2double(idx)+1;
-%     myfid1 = fopen(fullfile(inputfolder,myfiles(ii).name));
-%     data = textscan(myfid1,format); % round locations, there is a bug in estimation that shifts z-locations 0.5 pix. rounding results in better MSE
-%     fclose(myfid1);
-%     if size(data,2)==3
-%         des{ii} = [data{1} data{2} data{3}];
-%     else
-%         des{ii} = [[data{1} data{2} data{3}] data{4:numCols}];
-%     end
-% end
-% %%
-% %% TODO make it more than 2 channel
-% if numch>1
-%     pd2 = pdist2(des{1},des{2});
-%     [aa1,bb1] = min(pd2,[],1);
-%     [aa2,bb2] = min(pd2',[],1);% instead of min(pd2,[],2), make it row vector with transpose to prevent dimension error for single row entities
-%     bb1(aa1>1) = 1;
-%     bb2(aa2>1) = 1;
-%     keepthese = [1:length(bb1)]==bb2(bb1);
-%     des = des{2}(keepthese,:);
-% else
-%     des = des{end};
-% end
-% 
-% %%
-% 
-
-    % The original code for this seems very complicated.  Fuck all that.
+function des = readDesc(inputfolder, channel_index, ext_desc)
+    % The original code for this seems very complicated.  We'll so something simpler.
     
     % Parse args
-    % desc_ch is a cell array of strings.  But in all the use cases I've seen, the
-    % array is of length one.  And the string is either '0' or '1'.
-    if isempty(desc_ch) ,
+    if isempty(channel_index) ,
         error('readDesc() needs a channel index') ;
     end
-    channel_index_as_string = desc_ch{1} ;
-    channel_index = str2double(channel_index_as_string) ;
     if ~(channel_index==0 || channel_index==1) ,
         error('readDesc(): Channel must be 0 or 1') ;
     end
-    if nargin < 3 ,
+    if ~exist('ext_desc', 'var') || isempty(ext_desc) ,
         ext_desc = 'txt';
     end    
     
--- pointmatch.m
+++ pointmatch.m
@@ -1,174 +1,68 @@
-function varargout = pointmatch(tile1,tile2,acqusitionfolder1,acqusitionfolder2,outfold,pixshift,ch,maxnumofdesc,exitcode)
-
-% compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/compiledfunctions/pointmatch/pointmatch';
-% if ~exist(fileparts(compiledfunc),'dir')
-%     mkdir(fileparts(compiledfunc));
-%     mfilename_ = mfilename('fullpath');
-%     % unix(sprintf('mcc -m -v -R -singleCompThread %s -d %s -a %s',mfilename_,fileparts(compiledfunc),fullfile(fileparts(mfilename_),'functions')))
-%     unix(sprintf('mcc -m -v %s -d %s -a %s',mfilename_,fileparts(compiledfunc),fullfile(fileparts(mfilename_),'functions')))
-%     unix(sprintf('chmod g+x %s',compiledfunc))
-%     %,fullfile(fileparts(mfilename_),'common')
-% end
-% 
-% if ~isdeployed
-%     addpath(genpath('./functions'))
-% end
-
-% if nargin<1
-%     rawfolder = '/groups/mousebrainmicro/mousebrainmicro/data/' ;
-%     classifierfolder = '/nrs/mouselight/pipeline_output/' ;
-%     sample = '2018-08-15-skeltest' ;
-%     tileid1 = '/2018-08-18/00/00152' ;
-%     tileid2 = '/2018-08-18/00/00442' ;
-%     tile1 = fullfile(classifierfolder,sample,'/stage_2_descriptor_output',tileid1);
-%     tile2 = fullfile(classifierfolder,sample,'/stage_2_descriptor_output',tileid2);
-%     acqusitionfolder1 = fullfile(rawfolder,sample,'Tiling',tileid1);
-%     acqusitionfolder2 = fullfile(rawfolder,sample,'Tiling',tileid2);
-% end
-
-% if nargin<5
-%     outfold = tile1;
-%     pixshift = '[0 0 0]';
-%     ch='1';
-%     maxnumofdesc=1e3;
-%     exitcode = 0;
-if nargin < 6
-    pixshift = '[0 0 0]';
-    ch='1';
-    maxnumofdesc=1e3;
-    exitcode = 0;
-elseif nargin <7
-    ch='1';
-    maxnumofdesc=1e3;
-    exitcode = 0;
-elseif nargin <8
-    maxnumofdesc=1e3;
-    exitcode = 0;
-elseif nargin <9
-    exitcode = 0;
-end
-
-if ischar(pixshift)
-    pixshift = eval(pixshift); % pass initialization
-end
-if ischar(maxnumofdesc)
-    maxnumofdesc=str2double(maxnumofdesc);
-end
-if ischar(exitcode)
-    exitcode=str2double(exitcode);
-end
-
-if length(ch)>1
-    ch_desc={ch(1),ch(2)};
-else
-    ch_desc={ch};
-end
-varargout{1} = exitcode;
-dims = [1024,1536,251];
-projectionThr = 5;
-debug = 0;
-
-tag = 'XYZ';
-scopefile1 = readScopeFile(acqusitionfolder1);
-scopefile2 = readScopeFile(acqusitionfolder2);
-imsize_um = [scopefile1.x_size_um,scopefile1.y_size_um,scopefile1.z_size_um];
-% estimate translation
-gridshift = ([scopefile2.x scopefile2.y scopefile2.z]-[scopefile1.x scopefile1.y scopefile1.z]);
-iadj =find(gridshift);
-stgshift = 1000*([scopefile2.x_mm scopefile2.y_mm scopefile2.z_mm]-[scopefile1.x_mm scopefile1.y_mm scopefile1.z_mm]);
-if all(pixshift==0)
-    pixshift = round(stgshift.*(dims-1)./imsize_um);
-end
-
-%%
-% read descs
-desc1 = readDesc(tile1,ch_desc);
-desc2 = readDesc(tile2,ch_desc);
-% check if input exists
-if isempty(desc1) || isempty(desc2) ,
-    rate_ = 0;
-    X_ = [];
-    Y_ = [];
-    uni = 0;
-else
-    % correct images, xy flip
-    desc1 = correctTiles(desc1,dims);
-    desc2 = correctTiles(desc2,dims);
-    % truncate descriptors
-    desc1 = truncateDesc(desc1,maxnumofdesc);
-    desc2 = truncateDesc(desc2,maxnumofdesc);
-    if isempty(desc1) || isempty(desc2) ,
-        rate_ = 0;
-        X_ = [];
-        Y_ = [];
-        uni = 0;
-    else
-        % idaj : 1=right(+x), 2=bottom(+y), 3=below(+z)
-        % pixshift(iadj) = pixshift(iadj)+expensionshift(iadj); % initialize with a relative shift to improve CDP
-        matchparams = modelParams(projectionThr,debug);
-        if length(iadj)~=1 || max(iadj)>3
-            error('not 6 direction neighbor')
-        end
-        %% MATCHING
-        [X_,Y_,rate_,~,nonuniformity] = searchpair(desc1,desc2,pixshift,iadj,dims,matchparams);
-        if isempty(X_)
-            matchparams_ = matchparams;
-            matchparams_.opt.outliers = .5;
-            [X_,Y_,rate_,~,nonuniformity] = searchpair_relaxed(desc1(:,1:3),desc2(:,1:3),pixshift,iadj,dims,matchparams_);
-        end
-        
-        if ~isempty(X_)
-            X_ = correctTiles(X_,dims);
-            Y_ = correctTiles(Y_,dims);
-        end
-        uni = mean(nonuniformity)<=.5;
+function exitcode = pointmatch(tile1, tile2, acqusitionfolder1, acqusitionfolder2, output_folder_name, pixshift, ch, maxnumofdesc, exitcode)
+    % Deal with arguments
+    if ~exist('pixshift', 'var') || isempty(pixshift) ,
+        pixshift = '[0 0 0]' ;
     end
-end
-paireddescriptor.matchrate = rate_;
-paireddescriptor.X = X_;
-paireddescriptor.Y = Y_;
-paireddescriptor.uni = uni;
-if nargin>4
-    if ~isempty(X_)
-        %x:R, y:G, z:B
+    if ~exist('ch', 'var') || isempty(ch) ,
+        ch = '1' ;
+    end
+    if ~exist('maxnumofdesc', 'var') || isempty(maxnumofdesc) ,
+        maxnumofdesc=1e3 ;
+    end
+    if ~exist('exitcode', 'var') || isempty(exitcode) ,
+        exitcode = 0 ;
+    end
+    
+    % Eval args that are strings that we need to be numeric
+    if ischar(pixshift)
+        pixshift = eval(pixshift);
+    end
+    if ischar(ch) ,
+        ch = eval(ch) ;
+    end
+    if ischar(maxnumofdesc)
+        maxnumofdesc=str2double(maxnumofdesc);
+    end
+    if ischar(exitcode)
+        exitcode=str2double(exitcode);
+    end
+    
+    % Read in stuff from input files
+    scopefile1 = readScopeFile(acqusitionfolder1);
+    scopefile2 = readScopeFile(acqusitionfolder2);
+    desc1 = readDesc(tile1, ch) ;
+    desc2 = readDesc(tile2, ch) ;
+    
+    % Call the function that does the real work
+    [paireddescriptor, iadj] = pointmatch_core(desc1, desc2, scopefile1, scopefile2, pixshift, maxnumofdesc) ;
+
+    % Write the main output file
+    tag = 'XYZ';
+    output_file_name = fullfile(output_folder_name,sprintf('match-%s.mat',tag(iadj))); % append 1 if match found
+    if exist(output_file_name,'file')
+        unix(sprintf('rm -f %s',output_file_name)) ;
+    end
+    save(output_file_name,'paireddescriptor','scopefile1','scopefile2')
+    system(sprintf('chmod g+rw %s',output_file_name));
+    
+    % Write a thumbnail image file
+    X_ = paireddescriptor.X ;
+    Y_ = paireddescriptor.Y ;
+    % x:R, y:G, z:B
+    if isempty(X_) ,
+        col = [0 0 0] ;  % black as night
+    else
         col = median(Y_-X_,1)+128;
-        col = max(min(col,255),0);
-        outpng = zeros(105,89,3);
-        outpng(:,:,1) = col(1);
-        outpng(:,:,2) = col(2);
-        outpng(:,:,3) = col(3);
-        if exist(fullfile(outfold,'Thumbs.png'),'file')
-            % -f to prevent prompts
-            unix(sprintf('rm -f %s',fullfile(outfold,'Thumbs.png')));
-        end
-        imwrite(outpng,fullfile(outfold,'Thumbs.png'))
-        unix(sprintf('chmod g+rw %s',fullfile(outfold,'Thumbs.png')));
+    end    
+    col = max(min(col,255),0);
+    outpng = zeros(105,89,3);
+    outpng(:,:,1) = col(1);
+    outpng(:,:,2) = col(2);
+    outpng(:,:,3) = col(3);
+    thumbnail_image_file_name = fullfile(output_folder_name,'Thumbs.png') ;
+    if exist(thumbnail_image_file_name,'file')
+        system(sprintf('rm -f %s',thumbnail_image_file_name)) ;
     end
-    %%
-    if isempty(outfold)
-        varargout{2} = paireddescriptor;
-    else
-        % if isempty(rate_); val=0;elseif rate_<1;val=0;else;val=1;end
-        outputfile = fullfile(outfold,sprintf('match-%s.mat',tag(iadj))); % append 1 if match found
-        %check if file exist
-        if exist(outputfile,'file')
-            % if main match exists, crete a versioned one
-            outputfile1 = fullfile(outfold,sprintf('match-%s-1.mat',tag(iadj))); % append 1 if match found
-            save(outputfile1,'paireddescriptor','scopefile1','scopefile2')
-            unix(sprintf('chmod g+rw %s',outputfile1));
-        else
-            save(outputfile,'paireddescriptor','scopefile1','scopefile2')
-            unix(sprintf('chmod g+rw %s',outputfile));
-        end
-    end
+    imwrite(outpng,thumbnail_image_file_name)
+    system(sprintf('chmod g+rw %s',thumbnail_image_file_name));
 end
-
-
-
-
-
-
-
-
-
-
